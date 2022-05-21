6c28bdf7636ec257b97bca25256a900b803cd959
hamadlx <hamadismail112@gmail.com>
2022-05-22 04:52:29 +0600

    first commit

---
+++ instragram.php
@@ -0,0 +1,74 @@
+<section class="section ec-instagram-section module section-space-p" id="insta">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="section-title">
+                        <h2 class="ec-bg-title">Instagram Feed</h2>
+                        <h2 class="ec-title">Instagram Feed</h2>
+                        <p class="sub-title">Share your store with us</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ec-insta-wrapper">
+            <div class="ec-insta-outer">
+                <div class="container" data-animation="fadeIn">
+                    <div class="insta-auto">
+                        <!-- instagram item -->
+                        <div class="ec-insta-item">
+                            <div class="ec-insta-inner">
+                                <a href="#" target="_blank"><img src="assets/images/instragram-image/1.jpg"
+                                        alt="insta"></a>
+                            </div>
+                        </div>
+                        <!-- instagram item -->
+                        <div class="ec-insta-item">
+                            <div class="ec-insta-inner">
+                                <a href="#" target="_blank"><img src="assets/images/instragram-image/2.jpg"
+                                        alt="insta"></a>
+                            </div>
+                        </div>
+                        <!-- instagram item -->
+                        <div class="ec-insta-item">
+                            <div class="ec-insta-inner">
+                                <a href="#" target="_blank"><img src="assets/images/instragram-image/3.jpg"
+                                        alt="insta"></a>
+                            </div>
+                        </div>
+                        <!-- instagram item -->
+                        <div class="ec-insta-item">
+                            <div class="ec-insta-inner">
+                                <a href="#" target="_blank"><img src="assets/images/instragram-image/4.jpg"
+                                        alt="insta"></a>
+                            </div>
+                        </div>
+                        <!-- instagram item -->
+                        <!-- instagram item -->
+                        <div class="ec-insta-item">
+                            <div class="ec-insta-inner">
+                                <a href="#" target="_blank"><img src="assets/images/instragram-image/5.jpg"
+                                        alt="insta"></a>
+                            </div>
+                        </div>
+                        <!-- instagram item -->
+                        <!-- instagram item -->
+                        <div class="ec-insta-item">
+                            <div class="ec-insta-inner">
+                                <a href="#" target="_blank"><img src="assets/images/instragram-image/6.jpg"
+                                        alt="insta"></a>
+                            </div>
+                        </div>
+                        <!-- instagram item -->
+                        <!-- instagram item -->
+                        <div class="ec-insta-item">
+                            <div class="ec-insta-inner">
+                                <a href="#" target="_blank"><img src="assets/images/instragram-image/7.jpg"
+                                        alt="insta"></a>
+                            </div>
+                        </div>
+                        <!-- instagram item -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
